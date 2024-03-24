59d746a98a84178f3a6f0aee11f8bc43fdfbe01c
Abhik Mondal <142106053+Abhik2005@users.noreply.github.com>
2024-03-24 07:18:27 +0000

    Change

--- script.js
+++ script.js
@@ -143,7 +143,7 @@ function punjabiSongs() {
 }
 
 function musicPlay() {
-  var second = 0;
+  var second = 10;
   var minutes = 0;
   pause.style.display = "block";
   play.style.display = "none";
